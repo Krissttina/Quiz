98c6ed9ca31d393c41201433978e647e573e4501
Krissttina <krismetodieva123@gmail.com>
2022-06-12 14:36:33 +0300

    bug founded

--- code.js
+++ code.js
@@ -1,1 +1,34 @@
-document
+const que_text = document.querySelector(".que_text");
+
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
+    let option_tag = '<div class = "option"' + questions[index].options[0] + '<span></span></div>';
+                    + '<div class = "option"' + questions[index].options[1] + '<span></span></div>';
+                    + '<div class = "option"' + questions[index].options[2] + '<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option");
+    for(let i = 0; option.lenght; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+function answerSelected(answer){
+    let userAns = answer.textContent;
+    let correctAns = "Hyper Text Markup Language";
+    let allOptions = option_list.children.lenght;
+
+    if(userAns == correctAns){
+        answer.classList.add("correct");
+        console.log("Answer is correct!");
+    }else{
+        answer.classList.add("wrong");
+        console.log("Answer is wrong!");
+    }
+
+    for(let i = 0; i < allOptions; i++){
+        option_list.children[i].classList.add("disabled")
+    }
+}
---
+++ question.js
@@ -0,0 +1,12 @@
+let questions = [
+    {
+        numb:1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Home Tool Markup Language",
+            "Hyper Text Markup Language",
+            "Hyperlinks and Text Markup Language"
+        ]
+    }
+]
