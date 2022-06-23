b3e12f08c876eaad30af8eecb46f06319b7a9a93
Krissttina <krismetodieva123@gmail.com>
2022-06-23 13:39:33 +0300

    reset btn

--- code.js
+++ code.js
@@ -12,7 +12,7 @@
 //]
 
 const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const nextButton = document.getElementById('sub-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
