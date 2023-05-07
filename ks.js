ba3e583bb5d0680efd1b2707f86bfa80d8327bdd
Kristina <krismetodieva123@gmail.com>
2023-05-07 18:43:29 +0300

    update

---
+++ ks.js
@@ -0,0 +1,87 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('sub-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+    nextButton.classList.remove('hide')
+    startButton.classList('hide')
+  } else {
+    element.classList.add('wrong')
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+    nextButton.classList('hide')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What does HTML stand for?',
+    answers: [
+      { text: 'Hyper Text Markup Language', correct: true },
+      { text: 'Home Tool Markup Language', correct: false },
+      { text: 'Hyperlinks and Text Markup Language', correct: false }
+    ]
+  }
+]
--- script.js
+++ script.js
@@ -1,87 +1,54 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('sub-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+//input
+const startSection = document.getElementById('startSection');
+const questionSection = document.getElementById('QuestionSection');
+const winSection = document.getElementById('WinSection');
+const failSection = document.getElementById('FailSection');
 
-let shuffledQuestions, currentQuestionIndex
+const answerButtonsElement = document.getElementById('answerBtns')
 
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
+//start the quiz
+document.getElementById('startBtn').addEventListener('click', () => {
+  startSection.style.display = 'none';
+  questionSection.style.display = 'block';
+});
 
-function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
-}
-
-function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-  questionElement.innerText = question.question
-  question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
-    if (answer.correct) {
-      button.dataset.correct = answer.correct
-    }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
-}
-
-function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
-  while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
-function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
-  Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
-}
-
-function setStatusClass(element, correct) {
-  clearStatusClass(element)
-  if (correct) {
-    element.classList.add('correct')
-    nextButton.classList.remove('hide')
-    startButton.classList('hide')
-  } else {
-    element.classList.add('wrong')
-    startButton.innerText = 'Restart'
-    startButton.classList.remove('hide')
-    nextButton.classList('hide')
-  }
-}
-
-function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
-}
-
+//choose answear
 const questions = [
-  {
-    question: 'What does HTML stand for?',
-    answers: [
-      { text: 'Hyper Text Markup Language', correct: true },
-      { text: 'Home Tool Markup Language', correct: false },
-      { text: 'Hyperlinks and Text Markup Language', correct: false }
-    ]
+    {
+      question: 'What does HTML stand for?',
+      answers: [
+        { text: 'Hyper Text Markup Language', correct: true },
+        { text: 'Home Tool Markup Language', correct: false },
+        { text: 'Hyperlinks and Text Markup Language', correct: false }
+      ]
+    }
+  ]
+
+
+  function selectAnswer(event) {
+    const selectedButton = event.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
   }
-]
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+      nextButton.classList.remove('hide')
+      startButton.classList('hide')
+    } else {
+      element.classList.add('wrong')
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+      nextButton.classList('hide')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
